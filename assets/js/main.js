9eca5a8873cf92ac202074faf9bbb3cb5eae00c0
kabaneridev <48051231+kabaneridev@users.noreply.github.com>
2020-06-09 06:46:40 +0200

    add clock js

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,3 +41,15 @@
 			});
 
 })(jQuery);
+
+/* 
+	zegarek
+*/
+var myVar = setInterval(function() {
+  myTimer();
+}, 1000);
+
+function myTimer() {
+  var d = new Date();
+  document.getElementById("clock").innerHTML = d.toLocaleTimeString();
+}
